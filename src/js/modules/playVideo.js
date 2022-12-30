ebf3abdb30edb12b371444b945bd44b98f7a4df1
DoniyorShodiyevDoni <109288985+Doniyor2555@users.noreply.github.com>
2022-12-30 12:15:50 +0500

    Loan-Education

---
+++ src/js/modules/playVideo.js
@@ -0,0 +1,47 @@
+export default class VideoPlayer {
+  constructor(triggers, popup) {
+    this.btns = document.querySelectorAll(triggers);
+    this.overlay = document.querySelector(popup);
+    this.close = this.overlay.querySelector(".close");
+  }
+
+  bindTriggers() {
+    this.btns.forEach(btn => {
+      btn.addEventListener("click", () => {
+        if(document.querySelector("iframe#frame")){
+          this.overlay.style.display = 'flex';
+        } else {
+          const path = btn.getAttribute("data-url");
+          this.createPlayer(path);
+        }
+      });
+    });
+  }
+
+  bindCloseBtn() {
+   this.close.addEventListener("click", () => {
+    this.overlay.style.display = 'none';
+    this.player.stopVideo();
+   });
+  }
+
+  createPlayer(url) {
+    this.player = new YT.Player('frame', {
+      height: '100%',
+      width: '100%',
+      videoId: `${url}`
+    });
+    console.log(this.player);
+    this.overlay.style.display = 'flex';
+  }
+
+  init() {
+    let tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    let firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    this.bindTriggers();
+    this.bindCloseBtn();
+  }
+}
